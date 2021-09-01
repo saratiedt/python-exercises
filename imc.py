0e0d304a790451ac7ed48d75c7891a5cf0dd55d2
saratiedt <saracristinetiedt@gmail.com>
2021-09-01 20:04:42 -0300

    organização pastas

--- imc.py
+++
@@ -1,8 +0,0 @@
-nome = input("Digite seu nome: ")
-idade = input("Digite sua idade: ")
-altura = float(input("Digite sua altura: "))
-peso = float(input("Digite seu peso: "))
-
-imc = peso / (altura*2)
-
-print(nome, "voçê tem ", idade, "anos, e ", peso, "kilos e mede", altura, "metros. Seu IMC é", imc)
